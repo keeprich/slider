0b56408b434f9e55554b1f0d065a36ca3578e44c
keeprich <keeprich@keeprichs-air.lan>
2020-03-29 18:00:47 +0100

    adding more to slider

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+    $('#recipeCarousel').carousel({
+        interval: 10000
+      })
+      
+      $('.carousel .carousel-item1').each(function(){
+          var minPerSlide = 2;
+          var next = $(this).next();
+          if (!next.length) {
+          next = $(this).siblings(':first');
+          }
+          next.children(':first-child').clone().appendTo($(this));
+          
+          for (var i=0;i<minPerSlide;i++) {
+              next=next.next();
+              if (!next.length) {
+                  next = $(this).siblings(':first');
+                }
+              
+              next.children(':first-child').clone().appendTo($(this));
+            }
+      });
+      
